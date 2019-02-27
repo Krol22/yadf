59be60b021c643eca402228db338774993d7a464
Karol Toruń <Karol@brainhub.pl>
2019-02-27 13:14:25 +0100

    Added dozer and echo message

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 sudo gem install colorls
-brew install neovim
 brew install zsh
+brew install neovim
+brew cask install dozer
 sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 rm ~/.zshrc
 ln ~/.config/yadf/zshrc ~/.zshrc
+echo "You need still install VSCode, Spectacle, SwitchResX, Alfred";
