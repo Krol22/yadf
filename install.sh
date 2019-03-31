b983351ed506203ea5b73985d21991114616699e
Karol Torun <krol22ee@gmail.com>
2019-03-31 13:18:16 +0200

    Fixed aliases and antigen installation

--- install.sh
+++ install.sh
@@ -1,14 +1,21 @@
 sudo gem install colorls
+
 brew install zsh
 brew install neovim
 brew install node
 brew cask install iterm2
 brew cask install dozer
-sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-rm ~/.zshrc
-ln ~/.config/yadf/zshrc ~/.zshrc
-cp -r ./nvim/ ~/.config/nvim
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.config/nvim/bundle/Vundle.vim/
-echo "Run :PluginInstall from nvim to install all nvim plugins"
 
+sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+cp -r ./nvim $HOME/.config
+
+rm $HOME/.zshrc
+ln $HOME/.config/yadf/zshrc $HOME/.zshrc
+cp ./antigen.zsh $HOME/.antigen.zsh
+cp ./aliases.sh $HOME/.aliases.sh
+
+git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.config/nvim/bundle/Vundle.vim/
+
+echo "Run :PluginInstall from nvim to install all nvim plugins"
 echo "You need still install VSCode, Spectacle, SwitchResX, Alfred";
