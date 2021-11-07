afd6a7357973b2ba20f5debda72df6cbd809c069
Karol Torun <krol22ee@gmail.com>
2021-11-07 01:34:20 +0100

    fixing configuration

--- fish/config.fish
+++ fish/config.fish
@@ -11,20 +11,20 @@ alias vi="nvim"
 alias ls="colorls"
 alias l="colorls"
 
-alias ga="git add"
 alias gd="git diff"
-alias gc="git commit"
+alias gc="git commit -m"
 alias gs="git status"
-alias gp="git push"
+alias gp="git push origin"
 alias glp="gp -l"
+alias ga="git add"
 
-export PATH=~/.npm-global/bin:/usr/local/bin/mongod:/bin:/usr/bin:/usr/local/bin:{$PATH}
-export NODE_ENV='development'
-export POWERLEVEL9K_INSTALLATION_PATH=$ANTIGEN_BUNDLES/bhilburn/powerlevel9k
-export TERM=tmux-256color
-
+export PATH="~/.npm-global/bin:/usr/local/bin/mongod:/bin:/usr/bin:/usr/local/bin:$PATH"
 export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 export PATH="/Applications/Visual Studio Code.app/Contents/Resources/app/bin:$PATH"
 export PATH="/Applications/CMake.app/Contents/bin:$PATH"
 export PATH="/Users/krol22/Documents/Aseprite/aseprite/build/bin:$PATH"
 export PATH="/Users/krol22/Library/Python/3.8/bin:$PATH"
+
+export NODE_ENV='development'
+export TERM=tmux-256color
+
