84a70b496242ac79236f6d08861ea96afa517e54
Karol T <krol22ee@gmail.com>
2021-11-27 13:41:53 +0100

    (fish) - fish_add_path, aliases changes

--- fish/config.fish
+++ fish/config.fish
@@ -8,8 +8,8 @@ alias rm="rm -i"
 alias vim="nvim"
 alias vi="nvim"
 
-alias ls="colorls"
-alias l="colorls"
+alias ls="colorls --dark --group-directories-first"
+alias l="colorls --dark --group-directories-first"
 
 alias gd="git diff"
 alias gc="git commit"
@@ -18,14 +18,22 @@ alias gp="git push"
 alias glp="gp -l"
 alias ga="git add"
 
-export PATH="$HOME/.npm-global/bin:/usr/local/bin/mongod:/bin:/usr/bin:/usr/local/bin:$PATH"
-export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
-export PATH="/Applications/Visual Studio Code.app/Contents/Resources/app/bin:$PATH"
-export PATH="/Applications/CMake.app/Contents/bin:$PATH"
-export PATH="/Users/krol22/Documents/Aseprite/aseprite/build/bin:$PATH"
-export PATH="/Users/krol22/Library/Python/3.8/bin:$PATH"
+fish_add_path /bin
+fish_add_path /usr/bin
+fish_add_path /usr/local/bin
+fish_add_path /usr/local/bin/mongod
+fish_add_path ~/.npm-global/bin
+fish_add_path ~/.yarn/bin
+fish_add_path ~/.config/yarn/global/node_modules/.bin
+fish_add_path ~/Documents/Aseprite/aseprite/build/bin
+fish_add_path ~/Library/Python/3.8/bin
+fish_add_path ~/.platformio/penv/bin
+
+fish_add_path /Applications/Visual Studio Code.app/Contents/Resources/app/bin
+fish_add_path /Applications/CMake.app/Contents/bin
 
 export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
 export NODE_ENV='development'
 export TERM=tmux-256color
 
+set fish_greeting
